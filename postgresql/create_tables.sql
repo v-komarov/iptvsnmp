2b1ac9e34d5ba3ce266cf85f5fe7133e81efb8de
root <root@krsk-iptv-snmp.cttk.ttk.ru>
2019-11-21 11:52:55 +0700

    create tables

--- postgresql/create_tables.sql
+++ postgresql/create_tables.sql
@@ -7,3 +7,23 @@ CREATE TABLE IF NOT EXISTS circuit (
     status INTEGER DEFAULT 0
 );
 
+CREATE TABLE IF NOT EXISTS devices (
+    id INTEGER NOT NULL PRIMARY KEY,
+    vendor VARCHAR(50) NOT NULL,
+    device VARCHAR(100) NOT NULL,
+    snmp_ver VARCHAR(3) NOT NULL,
+    data jsonb NOT NULL
+);
+
+
+CREATE UNIQUE INDEX IF NOT EXISTS devices_u ON devices (vendor, device);
+CREATE INDEX IF NOT EXISTS devices_j ON devices USING GIN (data);
+
+
+
+CREATE TABLE IF NOT EXISTS addresses (
+    ip_address VARCHAR(15) NOT NULL PRIMARY KEY,
+    device INTEGER REFERENCES devices
+);
+
+
